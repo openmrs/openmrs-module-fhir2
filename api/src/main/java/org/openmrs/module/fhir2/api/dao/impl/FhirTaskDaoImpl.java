772d923de29c74c1d3f83984518af6ff69de1379
Ian <ian_bacher@brown.edu>
2020-01-13 12:08:52 -0500

    Ensure the proper license message is included in every file

--- api/src/main/java/org/openmrs/module/fhir2/api/dao/impl/FhirTaskDaoImpl.java
+++ api/src/main/java/org/openmrs/module/fhir2/api/dao/impl/FhirTaskDaoImpl.java
@@ -1,3 +1,12 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
 package org.openmrs.module.fhir2.api.dao.impl;
 
 import static org.hibernate.criterion.Restrictions.eq;
@@ -14,14 +23,6 @@ import org.openmrs.module.fhir2.api.dao.FhirTaskDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-
-/**
- * This class should not be used directly. <br>
- * The FhiTaskService should be used for all Task-related database manipulation.
- * 
- * @see org.openmrs.module.fhir2.api.FhirTaskService
- * @see org.openmrs.module.fhir2.api.dao.FhirTaskDao
- */
 
 @Component
 @Setter(AccessLevel.PACKAGE)
