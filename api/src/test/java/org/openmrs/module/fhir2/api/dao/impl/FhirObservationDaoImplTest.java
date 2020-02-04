576462a7f393c95cd69ea4fb38badfd251f3f69f
Ian <52504170+ibacher@users.noreply.github.com>
2020-02-04 09:39:31 -0500

    FM2-54 Improve Obs Search (#51)
    Co-authored-by: corneliouz bett <kbett68@gmail.com>

--- api/src/test/java/org/openmrs/module/fhir2/api/dao/impl/FhirObservationDaoImplTest.java
+++ api/src/test/java/org/openmrs/module/fhir2/api/dao/impl/FhirObservationDaoImplTest.java
@@ -17,6 +17,7 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
@@ -31,11 +32,14 @@ import ca.uhn.fhir.rest.api.SortOrderEnum;
 import ca.uhn.fhir.rest.api.SortSpec;
 import ca.uhn.fhir.rest.param.ReferenceParam;
 import ca.uhn.fhir.rest.param.TokenAndListParam;
+import ca.uhn.fhir.rest.param.TokenOrListParam;
 import ca.uhn.fhir.rest.param.TokenParam;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.hl7.fhir.r4.model.Patient;
 import org.junit.Before;
 import org.junit.Test;
 import org.openmrs.Obs;
+import org.openmrs.module.fhir2.FhirTestConstants;
 import org.openmrs.module.fhir2.TestFhirSpringConfiguration;
 import org.openmrs.test.BaseModuleContextSensitiveTest;
 import org.springframework.test.context.ContextConfiguration;
@@ -51,6 +55,8 @@ public class FhirObservationDaoImplTest extends BaseModuleContextSensitiveTest {
 	
 	private static final String OBS_CONCEPT_ID = "5089";
 	
+	private static final String OBS_CONCEPT_UUID = "c607c80f-1ea9-4da3-bb88-6276ce8868dd";
+	
 	private static final String PATIENT_UUID = "5946f880-b197-400b-9caa-a3c661d23041";
 	
 	private static final String PATIENT_GIVEN_NAME = "Collet";
@@ -64,6 +70,13 @@ public class FhirObservationDaoImplTest extends BaseModuleContextSensitiveTest {
 	private static final String SNOMED_SYSTEM_URI = "http://snomed.info/sct";
 	
 	private static final String OBS_SNOMED_CODE = "2332523";
+	
+	private static final String[] CIEL_VITAL_CODES = new String[] { "5085", "5086", "5087", "5088", "5089", "5090", "5092",
+	        "5242" };
+	
+	private static final String CIEL_DIASTOLIC_BP = "5086";
+	
+	private static final String LOINC_SYSTOLIC_BP = "8480-6";
 	
 	@Inject
 	FhirObservationDaoImpl dao;
@@ -103,6 +116,20 @@ public class FhirObservationDaoImplTest extends BaseModuleContextSensitiveTest {
 	}
 	
 	@Test
+	public void searchForObs_shouldSearchForObsByConceptUuid() {
+		TokenAndListParam code = new TokenAndListParam();
+		TokenParam codingToken = new TokenParam();
+		codingToken.setValue(OBS_CONCEPT_UUID);
+		code.addAnd(codingToken);
+		
+		Collection<Obs> results = dao.searchForObservations(null, null, code, null);
+		
+		assertThat(results, notNullValue());
+		assertThat(results, not(empty()));
+		assertThat(results, hasItem(hasProperty("uuid", equalTo(OBS_UUID))));
+	}
+	
+	@Test
 	public void searchForObs_shouldReturnObsByConceptMapping() {
 		TokenAndListParam code = new TokenAndListParam();
 		TokenParam codingToken = new TokenParam();
@@ -115,6 +142,79 @@ public class FhirObservationDaoImplTest extends BaseModuleContextSensitiveTest {
 		assertThat(results, notNullValue());
 		assertThat(results, not(empty()));
 		assertThat(results, hasItem(hasProperty("uuid", equalTo(OBS_UUID))));
+	}
+	
+	@Test
+	public void searchForObs_shouldReturnMultipleObsByConceptMapping() {
+		TokenAndListParam code = new TokenAndListParam();
+		TokenOrListParam orListParam = new TokenOrListParam();
+		code.addAnd(orListParam);
+		
+		for (String coding : CIEL_VITAL_CODES) {
+			TokenParam codingToken = new TokenParam();
+			codingToken.setSystem(FhirTestConstants.CIEL_SYSTEM_URN);
+			codingToken.setValue(coding);
+			orListParam.addOr(codingToken);
+		}
+		
+		Collection<Obs> results = dao.searchForObservations(null, null, code, null);
+		
+		assertThat(results, notNullValue());
+		assertThat(results, not(empty()));
+		assertThat(results, hasSize(greaterThan(1)));
+		for (String coding : CIEL_VITAL_CODES) {
+			assertThat(results,
+			    hasItem(hasProperty("concept", hasProperty("conceptId", equalTo(NumberUtils.toInt(coding))))));
+		}
+	}
+	
+	@Test
+	public void searchForObs_shouldReturnFromMultipleConceptMappings() {
+		TokenAndListParam code = new TokenAndListParam();
+		TokenOrListParam orListParam = new TokenOrListParam();
+		code.addAnd(orListParam);
+		
+		TokenParam codingToken1 = new TokenParam();
+		codingToken1.setSystem(FhirTestConstants.LOINC_SYSTEM_URL);
+		codingToken1.setValue(LOINC_SYSTOLIC_BP);
+		orListParam.addOr(codingToken1);
+		
+		TokenParam codingToken2 = new TokenParam();
+		codingToken2.setSystem(FhirTestConstants.CIEL_SYSTEM_URN);
+		codingToken2.setValue(CIEL_DIASTOLIC_BP);
+		orListParam.addOr(codingToken2);
+		
+		Collection<Obs> results = dao.searchForObservations(null, null, code, null);
+		
+		assertThat(results, notNullValue());
+		assertThat(results, not(empty()));
+		assertThat(results, hasSize(greaterThan(1)));
+		assertThat(results, hasItem(hasProperty("concept", hasProperty("conceptId", equalTo(5085)))));
+		assertThat(results, hasItem(hasProperty("concept", hasProperty("conceptId", equalTo(5086)))));
+	}
+	
+	@Test
+	public void searchForObs_shouldSupportMappedAndUnmappedConcepts() {
+		TokenAndListParam code = new TokenAndListParam();
+		TokenOrListParam orListParam = new TokenOrListParam();
+		code.addAnd(orListParam);
+		
+		TokenParam codingToken1 = new TokenParam();
+		codingToken1.setSystem(FhirTestConstants.LOINC_SYSTEM_URL);
+		codingToken1.setValue(LOINC_SYSTOLIC_BP);
+		orListParam.addOr(codingToken1);
+		
+		TokenParam codingToken2 = new TokenParam();
+		codingToken2.setValue(CIEL_DIASTOLIC_BP);
+		orListParam.addOr(codingToken2);
+		
+		Collection<Obs> results = dao.searchForObservations(null, null, code, null);
+		
+		assertThat(results, notNullValue());
+		assertThat(results, not(empty()));
+		assertThat(results, hasSize(greaterThan(1)));
+		assertThat(results, hasItem(hasProperty("concept", hasProperty("conceptId", equalTo(5085)))));
+		assertThat(results, hasItem(hasProperty("concept", hasProperty("conceptId", equalTo(5086)))));
 	}
 	
 	@Test
@@ -221,6 +321,67 @@ public class FhirObservationDaoImplTest extends BaseModuleContextSensitiveTest {
 		assertThat(results.size(), greaterThan(1));
 		
 		resultsList = new ArrayList<>(results);
+		// pair-wise compare of all obs by date
+		for (int i = 1; i < resultsList.size(); i++) {
+			assertThat(resultsList.get(i - 1).getObsDatetime(), sameOrAfter(resultsList.get(i).getObsDatetime()));
+		}
+	}
+	
+	@Test
+	public void searchForObs_shouldIgnoreSearchByUnknownProperty() {
+		SortSpec sort = new SortSpec();
+		sort.setParamName("date");
+		sort.setOrder(SortOrderEnum.DESC);
+		
+		Collection<Obs> baselineObs = dao.searchForObservations(null, null, null, sort);
+		
+		assertThat(baselineObs, notNullValue());
+		assertThat(baselineObs, not(empty()));
+		
+		SortSpec subSort = new SortSpec();
+		subSort.setParamName("dummy");
+		subSort.setOrder(SortOrderEnum.ASC);
+		sort.setChain(subSort);
+		
+		Collection<Obs> results = dao.searchForObservations(null, null, null, sort);
+		
+		assertThat(results, notNullValue());
+		assertThat(results, not(empty()));
+		assertThat(results, equalTo(baselineObs));
+	}
+	
+	@Test
+	public void searchForObs_shouldHandleComplexQuery() {
+		TokenAndListParam code = new TokenAndListParam();
+		TokenOrListParam orListParam = new TokenOrListParam();
+		code.addAnd(orListParam);
+		
+		for (String coding : CIEL_VITAL_CODES) {
+			TokenParam codingToken = new TokenParam();
+			codingToken.setSystem(FhirTestConstants.CIEL_SYSTEM_URN);
+			codingToken.setValue(coding);
+			orListParam.addOr(codingToken);
+		}
+		
+		ReferenceParam patientReference = new ReferenceParam();
+		patientReference.setChain("");
+		patientReference.setValue(PATIENT_UUID);
+		
+		SortSpec sort = new SortSpec();
+		sort.setParamName("date");
+		sort.setOrder(SortOrderEnum.DESC);
+		
+		Collection<Obs> results = dao.searchForObservations(null, patientReference, code, sort);
+		
+		assertThat(results, notNullValue());
+		assertThat(results, not(empty()));
+		assertThat(results, hasSize(greaterThan(1)));
+		for (String coding : CIEL_VITAL_CODES) {
+			assertThat(results,
+			    hasItem(hasProperty("concept", hasProperty("conceptId", equalTo(NumberUtils.toInt(coding))))));
+		}
+		
+		List<Obs> resultsList = new ArrayList<>(results);
 		// pair-wise compare of all obs by date
 		for (int i = 1; i < resultsList.size(); i++) {
 			assertThat(resultsList.get(i - 1).getObsDatetime(), sameOrAfter(resultsList.get(i).getObsDatetime()));
