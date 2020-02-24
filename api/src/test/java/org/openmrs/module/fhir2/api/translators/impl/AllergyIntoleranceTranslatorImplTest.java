55207a66b0a14784c11c718d85582d653dfab073
jecihjoy <jecihjoy@gmail.com>
2020-02-24 18:59:05 +0300

    FM2-84 : Additional properties to allergyIntolerance resource

--- api/src/test/java/org/openmrs/module/fhir2/api/translators/impl/AllergyIntoleranceTranslatorImplTest.java
+++ api/src/test/java/org/openmrs/module/fhir2/api/translators/impl/AllergyIntoleranceTranslatorImplTest.java
@@ -11,19 +11,32 @@ package org.openmrs.module.fhir2.api.translators.impl;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
+import static org.mockito.Mockito.when;
 
+import java.util.Date;
+
+import org.exparity.hamcrest.date.DateMatchers;
 import org.hl7.fhir.r4.model.AllergyIntolerance;
 import org.hl7.fhir.r4.model.CodeableConcept;
 import org.hl7.fhir.r4.model.Coding;
+import org.hl7.fhir.r4.model.Identifier;
+import org.hl7.fhir.r4.model.Reference;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.openmrs.Allergen;
 import org.openmrs.AllergenType;
 import org.openmrs.Allergy;
+import org.openmrs.Patient;
+import org.openmrs.User;
+import org.openmrs.module.fhir2.FhirTestConstants;
+import org.openmrs.module.fhir2.api.translators.PatientReferenceTranslator;
+import org.openmrs.module.fhir2.api.translators.PractitionerReferenceTranslator;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AllergyIntoleranceTranslatorImplTest {
@@ -34,6 +47,16 @@ public class AllergyIntoleranceTranslatorImplTest {
 	
 	private static final String ALLERGY_INTOLERANCE_CLINICAL_STATUC_INACTIVE = "inactive";
 	
+	private static final String PATIENT_UUID = "c0938432-1691-11df-9pa5-7038c432aaba";
+	
+	private static final String CREATOR_UUID = "c1038432-1691-11df-9pa5-7038c432aaba";
+	
+	@Mock
+	private PractitionerReferenceTranslator<User> practitionerReferenceTranslator;
+	
+	@Mock
+	private PatientReferenceTranslator patientReferenceTranslator;
+	
 	private AllergyIntoleranceTranslatorImpl allergyIntoleranceTranslator;
 	
 	private Allergy omrsAllergy;
@@ -41,6 +64,8 @@ public class AllergyIntoleranceTranslatorImplTest {
 	@Before
 	public void setUp() {
 		allergyIntoleranceTranslator = new AllergyIntoleranceTranslatorImpl();
+		allergyIntoleranceTranslator.setPractitionerReferenceTranslator(practitionerReferenceTranslator);
+		allergyIntoleranceTranslator.setPatientReferenceTranslator(patientReferenceTranslator);
 		omrsAllergy = new Allergy();
 	}
 	
@@ -120,6 +145,46 @@ public class AllergyIntoleranceTranslatorImplTest {
 	}
 	
 	@Test
+	public void toFhirResource_shouldTranslateOmrsPatientToFhirPatientReference() {
+		Patient patient = new Patient();
+		patient.setUuid(PATIENT_UUID);
+		omrsAllergy.setPatient(patient);
+		
+		Reference patientReference = new Reference().setReference(FhirTestConstants.PATIENT + "/" + PATIENT_UUID)
+		        .setType(FhirTestConstants.PATIENT).setIdentifier(new Identifier().setValue(PATIENT_UUID));
+		
+		when(patientReferenceTranslator.toFhirResource(patient)).thenReturn(patientReference);
+		AllergyIntolerance allergyIntolerance = allergyIntoleranceTranslator.toFhirResource(omrsAllergy);
+		assertThat(allergyIntolerance.getPatient(), notNullValue());
+		assertThat(allergyIntolerance.getPatient().getType(), is(FhirTestConstants.PATIENT));
+		assertThat(allergyIntoleranceTranslator.getReferenceId(allergyIntolerance.getPatient()), equalTo(PATIENT_UUID));
+	}
+	
+	@Test
+	public void toFhirResource_shouldTranslateOmrsAllergyCreatorToFhirPractitionerReference() {
+		User user = new User();
+		user.setUuid(CREATOR_UUID);
+		omrsAllergy.setCreator(user);
+		
+		Reference practionerReference = new Reference().setReference(FhirTestConstants.PRACTITIONER + "/" + CREATOR_UUID)
+		        .setType(FhirTestConstants.PRACTITIONER).setIdentifier(new Identifier().setValue(CREATOR_UUID));
+		
+		when(practitionerReferenceTranslator.toFhirResource(user)).thenReturn(practionerReference);
+		AllergyIntolerance allergyIntolerance = allergyIntoleranceTranslator.toFhirResource(omrsAllergy);
+		assertThat(allergyIntolerance.getRecorder(), notNullValue());
+		assertThat(allergyIntolerance.getRecorder().getType(), is(FhirTestConstants.PRACTITIONER));
+		assertThat(allergyIntoleranceTranslator.getReferenceId(allergyIntolerance.getRecorder()), equalTo(CREATOR_UUID));
+	}
+	
+	@Test
+	public void toFhirResource_shouldTranslateDateCreatedToRecordedDate() {
+		omrsAllergy.setDateCreated(new Date());
+		AllergyIntolerance allergyIntolerance = allergyIntoleranceTranslator.toFhirResource(omrsAllergy);
+		assertThat(allergyIntolerance, notNullValue());
+		assertThat(allergyIntolerance.getRecordedDate(), DateMatchers.sameDay(new Date()));
+	}
+	
+	@Test
 	public void toOpenmrsType_shouldTranslateAllergenTypeFoodCorrectly() {
 		AllergyIntolerance allergy = new AllergyIntolerance();
 		allergy.addCategory(AllergyIntolerance.AllergyIntoleranceCategory.FOOD);
@@ -156,7 +221,7 @@ public class AllergyIntoleranceTranslatorImplTest {
 	}
 	
 	@Test
-	public void toOpenmrsType_shouldReturnNullWhenCalledWithFhirNullCategory() {
+	public void toOpenmrsType_shouldReturnNullCategoryWhenCalledWithFhirNullCategory() {
 		AllergyIntolerance allergy = new AllergyIntolerance();
 		allergy.addCategory(AllergyIntolerance.AllergyIntoleranceCategory.NULL);
 		allergyIntoleranceTranslator.toOpenmrsType(omrsAllergy, allergy);
@@ -174,7 +239,7 @@ public class AllergyIntoleranceTranslatorImplTest {
 	}
 	
 	@Test
-	public void toOpenmrsType_shouldTranslateInactiveToVoidedFalse() {
+	public void toOpenmrsType_shouldTranslateActiveToVoidedFalse() {
 		AllergyIntolerance allergy = new AllergyIntolerance();
 		allergy.setClinicalStatus(
 		    new CodeableConcept().addCoding(new Coding("", ALLERGY_INTOLERANCE_CLINICAL_STATUC_ACTIVE, "")));
@@ -183,4 +248,47 @@ public class AllergyIntoleranceTranslatorImplTest {
 		assertThat(omrsAllergy.getVoided(), equalTo(false));
 	}
 	
+	@Test
+	public void toOpenmrsType_shouldTranslateRecordedDateToOpenmrsDateCreated() {
+		AllergyIntolerance allergy = new AllergyIntolerance();
+		allergy.setRecordedDate(new Date());
+		allergyIntoleranceTranslator.toOpenmrsType(omrsAllergy, allergy);
+		assertThat(omrsAllergy, notNullValue());
+		assertThat(omrsAllergy.getDateCreated(), DateMatchers.sameDay(new Date()));
+	}
+	
+	@Test
+	public void toOpenmrsType_shouldTranslatePatientReferenceToPatient() {
+		Reference patientReference = new Reference().setReference(FhirTestConstants.PATIENT + "/" + PATIENT_UUID)
+		        .setType(FhirTestConstants.PATIENT).setIdentifier(new Identifier().setValue(PATIENT_UUID));
+		AllergyIntolerance allergy = new AllergyIntolerance();
+		allergy.setPatient(patientReference);
+		
+		Patient omrsPatient = new Patient();
+		omrsPatient.setUuid(PATIENT_UUID);
+		when(patientReferenceTranslator.toOpenmrsType(patientReference)).thenReturn(omrsPatient);
+		
+		allergyIntoleranceTranslator.toOpenmrsType(omrsAllergy, allergy);
+		assertThat(omrsAllergy, notNullValue());
+		assertThat(omrsAllergy.getPatient(), is(omrsPatient));
+		assertThat(omrsAllergy.getPatient().getUuid(), is(PATIENT_UUID));
+	}
+	
+	@Test
+	public void toOpenmrsType_shouldTranslatePractitionerReferenceToCreator() {
+		Reference practitionerReference = new Reference().setReference(FhirTestConstants.PRACTITIONER + "/" + CREATOR_UUID)
+		        .setType(FhirTestConstants.PRACTITIONER).setIdentifier(new Identifier().setValue(CREATOR_UUID));
+		AllergyIntolerance allergy = new AllergyIntolerance();
+		allergy.setRecorder(practitionerReference);
+		
+		User user = new User();
+		user.setUuid(CREATOR_UUID);
+		omrsAllergy.setCreator(user);
+		when(practitionerReferenceTranslator.toOpenmrsType(practitionerReference)).thenReturn(user);
+		
+		allergyIntoleranceTranslator.toOpenmrsType(omrsAllergy, allergy);
+		assertThat(omrsAllergy, notNullValue());
+		assertThat(omrsAllergy.getCreator(), is(user));
+		assertThat(omrsAllergy.getCreator().getUuid(), is(CREATOR_UUID));
+	}
 }
