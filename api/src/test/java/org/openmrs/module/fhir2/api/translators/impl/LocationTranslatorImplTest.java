59c7eecf29649c22d6cae45f6f9e50a604e32eb0
jecihjoy <jecihjoy@gmail.com>
2020-03-23 17:49:02 +0300

    FM2-117 : Convert test assertion methods to assertThat() to improve code readability

--- api/src/test/java/org/openmrs/module/fhir2/api/translators/impl/LocationTranslatorImplTest.java
+++ api/src/test/java/org/openmrs/module/fhir2/api/translators/impl/LocationTranslatorImplTest.java
@@ -18,9 +18,6 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
@@ -123,66 +120,66 @@ public class LocationTranslatorImplTest {
 	@Test
 	public void shouldTranslateOpenmrsLocationToFhirLocation() {
 		org.hl7.fhir.r4.model.Location fhirLocation = locationTranslator.toFhirResource(omrsLocation);
-		assertNotNull(omrsLocation);
-		assertNotNull(fhirLocation);
-		assertEquals(omrsLocation.getName(), fhirLocation.getName());
+		assertThat(omrsLocation, notNullValue());
+		assertThat(fhirLocation, notNullValue());
+		assertThat(omrsLocation.getName(), equalTo(fhirLocation.getName()));
 	}
 	
 	@Test
 	public void shouldTranslateLocationUuidToFhiIdType() {
 		omrsLocation.setUuid(LOCATION_UUID);
 		org.hl7.fhir.r4.model.Location fhirLocation = locationTranslator.toFhirResource(omrsLocation);
-		assertNotNull(fhirLocation);
-		assertNotNull(fhirLocation.getId());
-		assertEquals(fhirLocation.getId(), LOCATION_UUID);
+		assertThat(fhirLocation, notNullValue());
+		assertThat(fhirLocation.getId(), notNullValue());
+		assertThat(fhirLocation.getId(), equalTo(LOCATION_UUID));
 	}
 	
 	@Test
 	public void shouldTranslateLocationNameToFhirNameType() {
 		omrsLocation.setName(LOCATION_NAME);
 		org.hl7.fhir.r4.model.Location fhirLocation = locationTranslator.toFhirResource(omrsLocation);
-		assertNotNull(fhirLocation);
-		assertNotNull(fhirLocation.getName());
-		assertEquals(fhirLocation.getName(), LOCATION_NAME);
+		assertThat(fhirLocation, notNullValue());
+		assertThat(fhirLocation.getName(), notNullValue());
+		assertThat(fhirLocation.getName(), equalTo(LOCATION_NAME));
 	}
 	
 	@Test
 	public void shouldTranslateLocationDescriptionToFhirDescriptionType() {
 		omrsLocation.setDescription(LOCATION_DESCRIPTION);
 		org.hl7.fhir.r4.model.Location fhirLocation = locationTranslator.toFhirResource(omrsLocation);
-		assertNotNull(fhirLocation);
-		assertNotNull(fhirLocation.getDescription());
-		assertEquals(fhirLocation.getDescription(), LOCATION_DESCRIPTION);
+		assertThat(fhirLocation, notNullValue());
+		assertThat(fhirLocation.getDescription(), notNullValue());
+		assertThat(fhirLocation.getDescription(), equalTo(LOCATION_DESCRIPTION));
 	}
 	
 	@Test
 	public void toFhirResource_shouldReturnEmptyLocationWhenCalledWitEmptyOmrsLocation() {
 		org.hl7.fhir.r4.model.Location location = locationTranslator.toFhirResource(null);
-		assertNull(location.getName());
-		assertNull(location.getDescription());
-		assertNull(location.getId());
-		assertNull(location.getPosition().getLatitude());
-		assertNull(location.getPosition().getLongitude());
-		assertNull(location.getAddress().getCity());
+		assertThat(location.getName(), nullValue());
+		assertThat(location.getDescription(), nullValue());
+		assertThat(location.getId(), nullValue());
+		assertThat(location.getPosition().getLatitude(), nullValue());
+		assertThat(location.getPosition().getLongitude(), nullValue());
+		assertThat(location.getAddress().getCity(), nullValue());
 	}
 	
 	@Test
 	public void toOpenmrsType_shouldReturnEmptyLocationWhenCalledWithEmptyFhirLocation() {
 		Location location = locationTranslator.toOpenmrsType(null);
-		assertNull(location.getName());
-		assertNull(location.getDescription());
-		assertNull(location.getId());
-		assertNull(location.getLatitude());
-		assertNull(location.getLongitude());
-		assertNull(location.getCountry());
+		assertThat(location.getName(), nullValue());
+		assertThat(location.getDescription(), nullValue());
+		assertThat(location.getId(), nullValue());
+		assertThat(location.getLatitude(), nullValue());
+		assertThat(location.getLongitude(), nullValue());
+		assertThat(location.getCountry(), nullValue());
 	}
 	
 	@Test
 	public void shouldTranslateFhirLocationToOmrsLocation() {
 		org.hl7.fhir.r4.model.Location location = new org.hl7.fhir.r4.model.Location();
 		org.openmrs.Location omrsLocation = locationTranslator.toOpenmrsType(location);
-		assertNotNull(omrsLocation);
-		assertEquals(omrsLocation.getName(), location.getName());
+		assertThat(omrsLocation, notNullValue());
+		assertThat(omrsLocation.getName(), equalTo(location.getName()));
 	}
 	
 	@Test
@@ -190,9 +187,9 @@ public class LocationTranslatorImplTest {
 		org.hl7.fhir.r4.model.Location location = new org.hl7.fhir.r4.model.Location();
 		location.setId(LOCATION_UUID);
 		org.openmrs.Location omrsLocation = locationTranslator.toOpenmrsType(location);
-		assertNotNull(omrsLocation);
-		assertNotNull(omrsLocation.getUuid());
-		assertEquals(omrsLocation.getUuid(), LOCATION_UUID);
+		assertThat(omrsLocation, notNullValue());
+		assertThat(omrsLocation.getUuid(), notNullValue());
+		assertThat(omrsLocation.getUuid(), equalTo(LOCATION_UUID));
 	}
 	
 	@Test
@@ -200,9 +197,9 @@ public class LocationTranslatorImplTest {
 		org.hl7.fhir.r4.model.Location location = new org.hl7.fhir.r4.model.Location();
 		location.setName(LOCATION_NAME);
 		org.openmrs.Location omrsLocation = locationTranslator.toOpenmrsType(location);
-		assertNotNull(omrsLocation);
-		assertNotNull(omrsLocation.getName());
-		assertEquals(omrsLocation.getName(), LOCATION_NAME);
+		assertThat(omrsLocation, notNullValue());
+		assertThat(omrsLocation.getName(), notNullValue());
+		assertThat(omrsLocation.getName(), equalTo(LOCATION_NAME));
 	}
 	
 	@Test
@@ -210,9 +207,9 @@ public class LocationTranslatorImplTest {
 		org.hl7.fhir.r4.model.Location location = new org.hl7.fhir.r4.model.Location();
 		location.setDescription(LOCATION_DESCRIPTION);
 		org.openmrs.Location omrsLocation = locationTranslator.toOpenmrsType(location);
-		assertNotNull(omrsLocation);
-		assertNotNull(omrsLocation.getDescription());
-		assertEquals(omrsLocation.getDescription(), LOCATION_DESCRIPTION);
+		assertThat(omrsLocation, notNullValue());
+		assertThat(omrsLocation.getDescription(), notNullValue());
+		assertThat(omrsLocation.getDescription(), equalTo(LOCATION_DESCRIPTION));
 	}
 	
 	@Test
@@ -220,26 +217,26 @@ public class LocationTranslatorImplTest {
 		omrsLocation.setLatitude(LOCATION_LATITUDE);
 		omrsLocation.setLongitude(LOCATION_LONGITUDE);
 		org.hl7.fhir.r4.model.Location fhirLocation = locationTranslator.toFhirResource(omrsLocation);
-		assertNotNull(fhirLocation);
-		assertNotNull(fhirLocation.getPosition());
-		assertNotNull(fhirLocation.getPosition().getLatitude());
-		assertNotNull(fhirLocation.getPosition().getLongitude());
+		assertThat(fhirLocation, notNullValue());
+		assertThat(fhirLocation.getPosition(), notNullValue());
+		assertThat(fhirLocation.getPosition().getLatitude(), notNullValue());
+		assertThat(fhirLocation.getPosition().getLongitude(), notNullValue());
 	}
 	
 	@Test
 	public void shouldSetFhirLocationToActiveIfLocationIsNotRetired() {
 		omrsLocation.setRetired(false);
 		org.hl7.fhir.r4.model.Location fhirLocation = locationTranslator.toFhirResource(omrsLocation);
-		assertNotNull(fhirLocation);
-		assertEquals(fhirLocation.getStatus(), org.hl7.fhir.r4.model.Location.LocationStatus.ACTIVE);
+		assertThat(fhirLocation, notNullValue());
+		assertThat(fhirLocation.getStatus(), equalTo(org.hl7.fhir.r4.model.Location.LocationStatus.ACTIVE));
 	}
 	
 	@Test
 	public void shouldSetFhirLocationToInActiveIfLocationIsRetired() {
 		omrsLocation.setRetired(true);
 		org.hl7.fhir.r4.model.Location fhirLocation = locationTranslator.toFhirResource(omrsLocation);
-		assertNotNull(fhirLocation);
-		assertEquals(fhirLocation.getStatus(), org.hl7.fhir.r4.model.Location.LocationStatus.INACTIVE);
+		assertThat(fhirLocation, notNullValue());
+		assertThat(fhirLocation.getStatus(), equalTo(org.hl7.fhir.r4.model.Location.LocationStatus.INACTIVE));
 	}
 	
 	@Test
