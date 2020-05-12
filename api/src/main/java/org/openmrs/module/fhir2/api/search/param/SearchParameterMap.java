cc7ccfd9813de168572ec35ed1aebdbb9a471d61
Siddharth Vaish <sidvaish97@gmail.com>
2020-05-12 23:55:31 +0530

    fix deployment issues

--- api/src/main/java/org/openmrs/module/fhir2/api/search/param/SearchParameterMap.java
+++ api/src/main/java/org/openmrs/module/fhir2/api/search/param/SearchParameterMap.java
@@ -35,7 +35,7 @@ public class SearchParameterMap implements Serializable {
 	@Getter
 	private SortSpec sortSpec;
 	
-	private LinkedHashMap<String, List<PropParam<?>>> params = new LinkedCaseInsensitiveMap<>();
+	private LinkedHashMap<String, List<PropParam<?>>> params = new LinkedHashMap<>();
 	
 	/**
 	 * Adds parameter into the {@link org.springframework.util.LinkedCaseInsensitiveMap}
