4de4810f10efe3acc03833ab11677a8515349857
Siddharth Vaish <sidvaish97@gmail.com>
2020-06-02 23:09:41 +0530

    client filter config

--- omod/src/main/java/org/openmrs/module/fhir2/web/filter/ClientAuthenticationFilter.java
+++ omod/src/main/java/org/openmrs/module/fhir2/web/filter/ClientAuthenticationFilter.java
@@ -14,11 +14,16 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
 import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.OidcKeycloakAccount;
 import org.keycloak.adapters.servlet.KeycloakOIDCFilter;
+import org.keycloak.adapters.spi.KeycloakAccount;
+import org.openmrs.api.context.Context;
 
 public class ClientAuthenticationFilter extends KeycloakOIDCFilter {
 	
@@ -37,6 +42,22 @@ public class ClientAuthenticationFilter extends KeycloakOIDCFilter {
 	
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+		if (req instanceof HttpServletRequest) {
+			HttpServletRequest httpRequest = (HttpServletRequest) req;
+			if (httpRequest.getRequestedSessionId() != null && !httpRequest.isRequestedSessionIdValid()) {
+				Context.logout();
+			}
+			if(httpRequest.getAttribute(KeycloakAccount.class.getName())!=null)
+			{
+				OidcKeycloakAccount account = (OidcKeycloakAccount) httpRequest.getAttribute(KeycloakAccount.class.getName());
+				Context.becomeUser(account.getPrincipal().getName());
+			}
+			else {
+				HttpServletResponse httpResponse = (HttpServletResponse) res;
+				httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Not authenticated");
+				return;
+			}
+		}
 		super.doFilter(req, res, chain);
 	}
 	
