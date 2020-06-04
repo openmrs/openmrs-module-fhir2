c88430031ec73e907c2c28229dab2668948c4324
Ian <ian_bacher@brown.edu>
2020-06-04 14:15:40 -0400

    Load Keycloak config from classpath

--- omod/src/main/java/org/openmrs/module/fhir2/web/filter/ClientAuthenticationFilter.java
+++ omod/src/main/java/org/openmrs/module/fhir2/web/filter/ClientAuthenticationFilter.java
@@ -18,41 +18,60 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.regex.Pattern;
 
-import org.keycloak.adapters.KeycloakConfigResolver;
-import org.keycloak.adapters.OidcKeycloakAccount;
+import lombok.extern.slf4j.Slf4j;
+import org.keycloak.adapters.AdapterDeploymentContext;
+import org.keycloak.adapters.KeycloakDeployment;
+import org.keycloak.adapters.KeycloakDeploymentBuilder;
+import org.keycloak.adapters.NodesRegistrationManagement;
 import org.keycloak.adapters.servlet.KeycloakOIDCFilter;
 import org.keycloak.adapters.spi.KeycloakAccount;
 import org.openmrs.api.context.Context;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
+@Slf4j
 public class ClientAuthenticationFilter extends KeycloakOIDCFilter {
 	
-	public ClientAuthenticationFilter(KeycloakConfigResolver definedconfigResolver) {
-		super(definedconfigResolver);
-	}
-	
-	public ClientAuthenticationFilter() {
-		super();
-	}
-	
 	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {
-		super.init(filterConfig);
+	public void init(FilterConfig filterConfig) {
+		final PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+		final Resource keycloakConfig = resolver.getResource("classpath*:keycloak.json");
+		final KeycloakDeployment deployment;
+		try {
+			deployment = KeycloakDeploymentBuilder.build(keycloakConfig.getInputStream());
+			this.deploymentContext = new AdapterDeploymentContext(deployment);
+		}
+		catch (IOException e) {
+			log.error("Error while trying to load Keycloak configuration", e);
+			this.deploymentContext = new AdapterDeploymentContext(new KeycloakDeployment());
+		}
+		
+		String skipPatternDefinition = filterConfig.getInitParameter(SKIP_PATTERN_PARAM);
+		if (skipPatternDefinition != null) {
+			skipPattern = Pattern.compile(skipPatternDefinition, Pattern.DOTALL);
+		}
+		
+		filterConfig.getServletContext().setAttribute(AdapterDeploymentContext.class.getName(), deploymentContext);
+		this.nodesRegistrationManagement = new NodesRegistrationManagement();
 	}
 	
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 		if (req instanceof HttpServletRequest) {
 			HttpServletRequest httpRequest = (HttpServletRequest) req;
+			
 			if (httpRequest.getAuthType() != null && !httpRequest.getAuthType().equals(HttpServletRequest.BASIC_AUTH)) {
-				
 				super.doFilter(req, res, chain);
+				
 				if (httpRequest.getRequestedSessionId() != null && !httpRequest.isRequestedSessionIdValid()) {
 					Context.logout();
 				}
+				
 				if (httpRequest.getAttribute(KeycloakAccount.class.getName()) != null) {
-					OidcKeycloakAccount account = (OidcKeycloakAccount) httpRequest
-					        .getAttribute(KeycloakAccount.class.getName());
+					KeycloakAccount account = (KeycloakAccount) httpRequest.getAttribute(KeycloakAccount.class.getName());
+					
 					Context.becomeUser(account.getPrincipal().getName());
 				} else {
 					if (!res.isCommitted()) {
