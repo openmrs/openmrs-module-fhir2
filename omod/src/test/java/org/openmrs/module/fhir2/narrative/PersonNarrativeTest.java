6367c1e2bc79c6b514d2fc775c0b53184e4b8013
Shivansh Rakesh <iamshivansh.2000@gmail.com>
2020-06-30 01:05:35 +0530

    FM2-208: Create default narratives for Person, Practitioner and ServiceRequest (#219)

---
+++ omod/src/test/java/org/openmrs/module/fhir2/narrative/PersonNarrativeTest.java
@@ -0,0 +1,39 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.fhir2.narrative;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+import java.io.IOException;
+
+import org.hl7.fhir.r4.model.Person;
+import org.junit.Test;
+
+public class PersonNarrativeTest extends BaseFhirNarrativeTest {
+	
+	private static final String EXAMPLE_RESOURCE_PATH = "org/openmrs/module/fhir2/narrative/example/person-example.json";
+	
+	private static final String EXPECTED_NARRATIVE_PATH = "org/openmrs/module/fhir2/narrative/expected/person-expected.html";
+	
+	@Test
+	public void shouldGeneratePersonNarrative() throws IOException {
+		Person given = parser.parseResource(Person.class,
+		    getClass().getClassLoader().getResourceAsStream(EXAMPLE_RESOURCE_PATH));
+		
+		Person result = parser.parseResource(Person.class, parser.encodeResourceToString(given));
+		
+		assertThat(result, notNullValue());
+		assertThat(result.getText(), notNullValue());
+		assertThat(result.getText().getStatusAsString(), equalTo("generated"));
+		assertThat(result.getText().getDivAsString(), equalTo(readNarrativeFile(EXPECTED_NARRATIVE_PATH)));
+	}
+}
