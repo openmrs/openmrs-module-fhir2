5369ffc1b59b6b2cc4f46baa93425d4fb95bfb51
theanandankit <ankitanand2909@gmail.com>
2020-12-13 01:16:03 +0530

    liquibase modified

--- api/src/main/java/org/openmrs/module/fhir2/api/translators/impl/DurationUnitTranslatorImpl.java
+++ api/src/main/java/org/openmrs/module/fhir2/api/translators/impl/DurationUnitTranslatorImpl.java
@@ -1,36 +1,36 @@
-package org.openmrs.module.fhir2.api.translators.impl;
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
 
-import org.hl7.fhir.r4.model.Timing;
-import org.openmrs.DrugOrder;
-import org.openmrs.module.fhir2.api.translators.DurationUnitTranslator;
+package org.openmrs.module.fhir2.api.translators.impl;
 
 import javax.annotation.Nonnull;
 
-public class DurationUnitTranslatorImpl implements DurationUnitTranslator {
+import org.hl7.fhir.r4.model.Timing;
+import org.openmrs.DrugOrder;
+import org.openmrs.module.fhir2.api.mappings.DurationUnitMap;
+import org.openmrs.module.fhir2.api.translators.DurationUnitTranslator;
+import org.springframework.beans.factory.annotation.Autowired;
 
+public class DurationUnitTranslatorImpl implements DurationUnitTranslator {
+	
+	@Autowired
+	private DurationUnitMap durationUnitMap;
+	
 	@Override
 	public Timing.UnitsOfTime toFhirResource(@Nonnull DrugOrder drugOrder) {
-
-		String unitUUID = drugOrder.getDurationUnits().getUuid();
-
-		switch (unitUUID) {
-
-			case "162583":
-				return Timing.UnitsOfTime.S;
-			case "1733":
-				return Timing.UnitsOfTime.MIN;
-			case "1822":
-				return Timing.UnitsOfTime.H;
-			case "1072":
-				return Timing.UnitsOfTime.D;
-			case "1073":
-				return Timing.UnitsOfTime.WK;
-			case "1074":
-				return Timing.UnitsOfTime.MO;
-			case "1734":
-				return Timing.UnitsOfTime.A;
-			default:
-				return Timing.UnitsOfTime.NULL;
+		
+		if (drugOrder.getDurationUnits().getUuid() == null) {
+			return null;
 		}
+		
+		return durationUnitMap.getDurationUnit(drugOrder.getDurationUnits().getUuid());
+		
 	}
 }
