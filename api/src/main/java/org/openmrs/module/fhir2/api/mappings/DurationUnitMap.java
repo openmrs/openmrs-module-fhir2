355e5ce87e46f9d15a66d6f2d9e5a0fa6692f5a5
Ankit kumar <49350053+theanandankit@users.noreply.github.com>
2021-02-01 20:16:47 +0530

    FM-189: Add Timing.TimingRepeatComponent.DurationUnit property (#310)

---
+++ api/src/main/java/org/openmrs/module/fhir2/api/mappings/DurationUnitMap.java
@@ -0,0 +1,49 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.fhir2.api.mappings;
+
+import static org.hibernate.criterion.Restrictions.eq;
+
+import javax.annotation.Nonnull;
+
+import lombok.AccessLevel;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.NonUniqueResultException;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Projections;
+import org.hl7.fhir.r4.model.Timing;
+import org.openmrs.module.fhir2.model.FhirDurationUnitMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@Setter(AccessLevel.PUBLIC)
+public class DurationUnitMap {
+	
+	@Autowired
+	@Qualifier("sessionFactory")
+	private SessionFactory sessionFactory;
+	
+	public Timing.UnitsOfTime getDurationUnit(@Nonnull String conceptUuid) {
+		
+		try {
+			return (Timing.UnitsOfTime) sessionFactory.getCurrentSession().createCriteria(FhirDurationUnitMap.class)
+			        .createAlias("concept", "c").add(eq("c.uuid", conceptUuid))
+			        .setProjection(Projections.property("unit_of_time")).uniqueResult();
+		}
+		catch (NonUniqueResultException e) {
+			log.error("Exception caught while trying to load DurationUnit for concept '{}'", conceptUuid, e);
+		}
+		return null;
+	}
+}
---
+++ api/src/main/java/org/openmrs/module/fhir2/model/FhirDurationUnitMap.java
@@ -0,0 +1,90 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.fhir2.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import java.util.Date;
+import java.util.UUID;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.search.annotations.Field;
+import org.hl7.fhir.r4.model.Timing;
+import org.openmrs.Auditable;
+import org.openmrs.Concept;
+import org.openmrs.Retireable;
+import org.openmrs.User;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
+@Entity
+@Table(name = "fhir_duration_unit_map")
+public class FhirDurationUnitMap implements Auditable, Retireable {
+	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "duration_unit_map_id")
+	private Integer id;
+	
+	@ManyToOne(optional = false)
+	@JoinColumn(nullable = false, name = "concept_id")
+	private Concept concept;
+	
+	@Column(nullable = false, name = "unit_of_time")
+	@Enumerated(EnumType.STRING)
+	private Timing.UnitsOfTime unit_of_time;
+	
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "creator", updatable = false)
+	protected User creator;
+	
+	@Column(name = "date_created", nullable = false, updatable = false)
+	private Date dateCreated;
+	
+	@ManyToOne
+	@JoinColumn(name = "changed_by")
+	private User changedBy;
+	
+	@Column(name = "date_changed")
+	private Date dateChanged;
+	
+	@Column(name = "retired", nullable = false)
+	@Field
+	private Boolean retired = Boolean.FALSE;
+	
+	@Column(name = "date_retired")
+	private Date dateRetired;
+	
+	@ManyToOne
+	@JoinColumn(name = "retired_by")
+	private User retiredBy;
+	
+	@Column(name = "retire_reason")
+	private String retireReason;
+	
+	@Column(name = "uuid", unique = true, nullable = false, length = 36)
+	private String uuid = UUID.randomUUID().toString();
+	
+	@Override
+	public Boolean isRetired() {
+		return retired;
+	}
+}
