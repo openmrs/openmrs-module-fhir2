94f019e35656f1f4e0394458a2da5c787d2d30eb
Ian <ian_bacher@brown.edu>
2021-04-27 16:05:24 -0400

    Formatting fix

--- api/src/main/java/org/openmrs/module/fhir2/api/util/FhirUtils.java
+++ api/src/main/java/org/openmrs/module/fhir2/api/util/FhirUtils.java
@@ -23,85 +23,85 @@ import org.hl7.fhir.r4.model.Reference;
 import org.openmrs.module.fhir2.FhirConstants;
 
 public class FhirUtils {
-
+	
 	public enum OpenmrsEncounterType {
 		ENCOUNTER,
 		VISIT,
 		AMBIGUOUS
 	}
-
+	
 	// see https://www.hl7.org/fhir/references.html#literal
 	private static final Pattern FHIR_URL = Pattern.compile("(?:(?:http|https)://(?:[A-Za-z0-9\\-.:%$\\\\]*/)+)?"
-			+ "(?<type>Account|ActivityDefinition|AdverseEvent|AllergyIntolerance|Appointment|AppointmentResponse"
-			+ "|AuditEvent|Basic|Binary|BiologicallyDerivedProduct|BodyStructure|Bundle|CapabilityStatement|CarePlan"
-			+ "|CareTeam|CatalogEntry|ChargeItem|ChargeItemDefinition|Claim|ClaimResponse|ClinicalImpression"
-			+ "|CodeSystem|Communication|CommunicationRequest|CompartmentDefinition|Composition|ConceptMap"
-			+ "|Condition|Consent|Contract|Coverage|CoverageEligibilityRequest|CoverageEligibilityResponse"
-			+ "|DetectedIssue|Device|DeviceDefinition|DeviceMetric|DeviceRequest|DeviceUseStatement"
-			+ "|DiagnosticReport|DocumentManifest|DocumentReference|EffectEvidenceSynthesis|Encounter|Endpoint"
-			+ "|EnrollmentRequest|EnrollmentResponse|EpisodeOfCare|EventDefinition|Evidence|EvidenceVariable"
-			+ "|ExampleScenario|ExplanationOfBenefit|FamilyMemberHistory|Flag|Goal|GraphDefinition|Group"
-			+ "|GuidanceResponse|HealthcareService|ImagingStudy|Immunization|ImmunizationEvaluation"
-			+ "|ImmunizationRecommendation|ImplementationGuide|InsurancePlan|Invoice|Library|Linkage|List|Location"
-			+ "|Measure|MeasureReport|Media|Medication|MedicationAdministration|MedicationDispense"
-			+ "|MedicationKnowledge|MedicationRequest|MedicationStatement|MedicinalProduct"
-			+ "|MedicinalProductAuthorization|MedicinalProductContraindication|MedicinalProductIndication"
-			+ "|MedicinalProductIngredient|MedicinalProductInteraction|MedicinalProductManufactured"
-			+ "|MedicinalProductPackaged|MedicinalProductPharmaceutical|MedicinalProductUndesirableEffect"
-			+ "|MessageDefinition|MessageHeader|MolecularSequence|NamingSystem|NutritionOrder|Observation"
-			+ "|ObservationDefinition|OperationDefinition|OperationOutcome|Organization|OrganizationAffiliation"
-			+ "|Patient|PaymentNotice|PaymentReconciliation|Person|PlanDefinition|Practitioner|PractitionerRole"
-			+ "|Procedure|Provenance|Questionnaire|QuestionnaireResponse|RelatedPerson|RequestGroup"
-			+ "|ResearchDefinition|ResearchElementDefinition|ResearchStudy|ResearchSubject|RiskAssessment"
-			+ "|RiskEvidenceSynthesis|Schedule|SearchParameter|ServiceRequest|Slot|Specimen|SpecimenDefinition"
-			+ "|StructureDefinition|StructureMap|Subscription|Substance|SubstanceNucleicAcid|SubstancePolymer"
-			+ "|SubstanceProtein|SubstanceReferenceInformation|SubstanceSourceMaterial|SubstanceSpecification"
-			+ "|SupplyDelivery|SupplyRequest|Task|TerminologyCapabilities|TestReport|TestScript|ValueSet"
-			+ "|VerificationResult|VisionPrescription)"
-			+ "/(?<id>[A-Za-z0-9\\-.]{1,64})(?:/_history/(?<version>[A-Za-z0-9\\-.]{1,64}))?");
-
+	        + "(?<type>Account|ActivityDefinition|AdverseEvent|AllergyIntolerance|Appointment|AppointmentResponse"
+	        + "|AuditEvent|Basic|Binary|BiologicallyDerivedProduct|BodyStructure|Bundle|CapabilityStatement|CarePlan"
+	        + "|CareTeam|CatalogEntry|ChargeItem|ChargeItemDefinition|Claim|ClaimResponse|ClinicalImpression"
+	        + "|CodeSystem|Communication|CommunicationRequest|CompartmentDefinition|Composition|ConceptMap"
+	        + "|Condition|Consent|Contract|Coverage|CoverageEligibilityRequest|CoverageEligibilityResponse"
+	        + "|DetectedIssue|Device|DeviceDefinition|DeviceMetric|DeviceRequest|DeviceUseStatement"
+	        + "|DiagnosticReport|DocumentManifest|DocumentReference|EffectEvidenceSynthesis|Encounter|Endpoint"
+	        + "|EnrollmentRequest|EnrollmentResponse|EpisodeOfCare|EventDefinition|Evidence|EvidenceVariable"
+	        + "|ExampleScenario|ExplanationOfBenefit|FamilyMemberHistory|Flag|Goal|GraphDefinition|Group"
+	        + "|GuidanceResponse|HealthcareService|ImagingStudy|Immunization|ImmunizationEvaluation"
+	        + "|ImmunizationRecommendation|ImplementationGuide|InsurancePlan|Invoice|Library|Linkage|List|Location"
+	        + "|Measure|MeasureReport|Media|Medication|MedicationAdministration|MedicationDispense"
+	        + "|MedicationKnowledge|MedicationRequest|MedicationStatement|MedicinalProduct"
+	        + "|MedicinalProductAuthorization|MedicinalProductContraindication|MedicinalProductIndication"
+	        + "|MedicinalProductIngredient|MedicinalProductInteraction|MedicinalProductManufactured"
+	        + "|MedicinalProductPackaged|MedicinalProductPharmaceutical|MedicinalProductUndesirableEffect"
+	        + "|MessageDefinition|MessageHeader|MolecularSequence|NamingSystem|NutritionOrder|Observation"
+	        + "|ObservationDefinition|OperationDefinition|OperationOutcome|Organization|OrganizationAffiliation"
+	        + "|Patient|PaymentNotice|PaymentReconciliation|Person|PlanDefinition|Practitioner|PractitionerRole"
+	        + "|Procedure|Provenance|Questionnaire|QuestionnaireResponse|RelatedPerson|RequestGroup"
+	        + "|ResearchDefinition|ResearchElementDefinition|ResearchStudy|ResearchSubject|RiskAssessment"
+	        + "|RiskEvidenceSynthesis|Schedule|SearchParameter|ServiceRequest|Slot|Specimen|SpecimenDefinition"
+	        + "|StructureDefinition|StructureMap|Subscription|Substance|SubstanceNucleicAcid|SubstancePolymer"
+	        + "|SubstanceProtein|SubstanceReferenceInformation|SubstanceSourceMaterial|SubstanceSpecification"
+	        + "|SupplyDelivery|SupplyRequest|Task|TerminologyCapabilities|TestReport|TestScript|ValueSet"
+	        + "|VerificationResult|VisionPrescription)"
+	        + "/(?<id>[A-Za-z0-9\\-.]{1,64})(?:/_history/(?<version>[A-Za-z0-9\\-.]{1,64}))?");
+	
 	public static String newUuid() {
 		return UUID.randomUUID().toString();
 	}
-
+	
 	public static Optional<String> getReferenceType(Reference reference) {
 		if (reference == null || !(reference.hasType() || reference.hasReference())) {
 			return Optional.empty();
 		}
-
+		
 		if (reference.hasType()) {
 			return Optional.of(reference.getType());
 		} else {
 			return referenceToType(reference.getReference());
 		}
 	}
-
+	
 	public static Optional<String> referenceToType(String reference) {
 		if (StringUtils.isBlank(reference)) {
 			return Optional.empty();
 		}
-
+		
 		Matcher m = FHIR_URL.matcher(reference);
 		if (m.matches()) {
 			return Optional.of(m.group("type"));
 		}
-
+		
 		return Optional.empty();
 	}
-
+	
 	public static Optional<String> referenceToId(String reference) {
 		if (StringUtils.isBlank(reference)) {
 			return Optional.empty();
 		}
-
+		
 		Matcher m = FHIR_URL.matcher(reference);
 		if (m.matches()) {
 			return Optional.of(m.group("id"));
 		}
-
+		
 		return Optional.empty();
 	}
-
+	
 	public static OperationOutcome createExceptionErrorOperationOutcome(String diagnostics) {
 		OperationOutcome outcome = new OperationOutcome();
 		OperationOutcome.OperationOutcomeIssueComponent issue = outcome.addIssue();
@@ -110,21 +110,19 @@ public class FhirUtils {
 		issue.setDiagnostics(diagnostics);
 		return outcome;
 	}
-
+	
 	public static Optional<OpenmrsEncounterType> getOpenmrsEncounterType(Encounter encounter) {
-		List<OpenmrsEncounterType> openmrsEncounterTypes = encounter.getType().stream().flatMap(it ->
-				it.getCoding().stream()
-						.filter(coding -> FhirConstants.ENCOUNTER_TYPE_SYSTEM_URI.equals(coding.getSystem()) ||
-								FhirConstants.VISIT_TYPE_SYSTEM_URI.equals(coding.getSystem()))
-						.map(coding -> {
-							if (FhirConstants.ENCOUNTER_TYPE_SYSTEM_URI.equals(coding.getSystem())) {
-								return OpenmrsEncounterType.ENCOUNTER;
-							} else {
-								return OpenmrsEncounterType.VISIT;
-							}
-						})
-		).distinct().collect(Collectors.toList());
-
+		List<OpenmrsEncounterType> openmrsEncounterTypes = encounter.getType().stream().flatMap(
+		    it -> it.getCoding().stream().filter(coding -> FhirConstants.ENCOUNTER_TYPE_SYSTEM_URI.equals(coding.getSystem())
+		            || FhirConstants.VISIT_TYPE_SYSTEM_URI.equals(coding.getSystem())).map(coding -> {
+			            if (FhirConstants.ENCOUNTER_TYPE_SYSTEM_URI.equals(coding.getSystem())) {
+				            return OpenmrsEncounterType.ENCOUNTER;
+			            } else {
+				            return OpenmrsEncounterType.VISIT;
+			            }
+		            }))
+		        .distinct().collect(Collectors.toList());
+		
 		if (openmrsEncounterTypes.isEmpty()) {
 			return Optional.empty();
 		} else if (openmrsEncounterTypes.size() == 1) {
