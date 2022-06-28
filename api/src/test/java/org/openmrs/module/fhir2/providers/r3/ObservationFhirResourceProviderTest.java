5f3b66fbef0b9b8f501a484dcf378b6d0959e993
Muta-Jonathan <mutajonathan4@gmalil.com>
2022-06-28 16:52:39 +0300

    added missing Arg

--- api/src/test/java/org/openmrs/module/fhir2/providers/r3/ObservationFhirResourceProviderTest.java
+++ api/src/test/java/org/openmrs/module/fhir2/providers/r3/ObservationFhirResourceProviderTest.java
@@ -184,7 +184,7 @@ public class ObservationFhirResourceProviderTest extends BaseFhirR3ProvenanceRes
 		ReferenceAndListParam encounterParam = new ReferenceAndListParam();
 		encounterParam.addValue(new ReferenceOrListParam().add(new ReferenceParam().setChain(Encounter.SP_TYPE)));
 		
-		IBundleProvider results = resourceProvider.searchObservations(null, null, null, null, null, null, null, null, null,
+		IBundleProvider results = resourceProvider.searchObservations(encounterParam, null, null, null, null, null, null, null, null,
 		    null, null, null, null, null, null, null);
 		
 		List<IBaseResource> resultList = get(results, 1, 5);
