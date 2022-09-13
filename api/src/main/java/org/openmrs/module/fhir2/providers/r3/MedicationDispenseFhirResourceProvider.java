61b51182a469907d7a67758e9d7db92c66390cd3
Samuel Male <samuelsmalek@gmail.com>
2022-09-13 21:16:33 +0300

    Update MedicationDispense providers with necessary annotations (#427)

--- api/src/main/java/org/openmrs/module/fhir2/providers/r3/MedicationDispenseFhirResourceProvider.java
+++ api/src/main/java/org/openmrs/module/fhir2/providers/r3/MedicationDispenseFhirResourceProvider.java
@@ -16,6 +16,8 @@ import javax.annotation.Nonnull;
 import java.util.HashSet;
 
 import ca.uhn.fhir.model.api.Include;
+import ca.uhn.fhir.rest.annotation.Create;
+import ca.uhn.fhir.rest.annotation.Delete;
 import ca.uhn.fhir.rest.annotation.IdParam;
 import ca.uhn.fhir.rest.annotation.IncludeParam;
 import ca.uhn.fhir.rest.annotation.OptionalParam;
@@ -23,6 +25,7 @@ import ca.uhn.fhir.rest.annotation.Read;
 import ca.uhn.fhir.rest.annotation.ResourceParam;
 import ca.uhn.fhir.rest.annotation.Search;
 import ca.uhn.fhir.rest.annotation.Sort;
+import ca.uhn.fhir.rest.annotation.Update;
 import ca.uhn.fhir.rest.api.MethodOutcome;
 import ca.uhn.fhir.rest.api.SortSpec;
 import ca.uhn.fhir.rest.api.server.IBundleProvider;
@@ -72,13 +75,15 @@ public class MedicationDispenseFhirResourceProvider implements IResourceProvider
 		}
 		return MedicationDispense30_40.convertMedicationDispense(r4Obj);
 	}
-	
+
+	@Create
 	public MethodOutcome createMedicationDispense(@ResourceParam MedicationDispense mDispense) {
 		org.hl7.fhir.r4.model.MedicationDispense r4Obj = fhirMedicationDispenseService
 		        .create(MedicationDispense30_40.convertMedicationDispense(mDispense));
 		return FhirProviderUtils.buildCreate(MedicationDispense30_40.convertMedicationDispense(r4Obj));
 	}
-	
+
+	@Update
 	public MethodOutcome updateMedicationDispense(@IdParam IdType id, @ResourceParam MedicationDispense mDispense) {
 		if (id == null || id.getIdPart() == null) {
 			throw new InvalidRequestException("id must be specified to update resource");
@@ -88,7 +93,8 @@ public class MedicationDispenseFhirResourceProvider implements IResourceProvider
 		    MedicationDispense30_40.convertMedicationDispense(mDispense));
 		return FhirProviderUtils.buildUpdate(r4Obj);
 	}
-	
+
+	@Delete
 	public OperationOutcome deleteMedicationDispense(@IdParam IdType id) {
 		fhirMedicationDispenseService.delete(id.getIdPart());
 		return FhirProviderUtils.buildDeleteR3();
--- api/src/main/java/org/openmrs/module/fhir2/providers/r4/MedicationDispenseFhirResourceProvider.java
+++ api/src/main/java/org/openmrs/module/fhir2/providers/r4/MedicationDispenseFhirResourceProvider.java
@@ -16,6 +16,8 @@ import javax.annotation.Nonnull;
 import java.util.HashSet;
 
 import ca.uhn.fhir.model.api.Include;
+import ca.uhn.fhir.rest.annotation.Create;
+import ca.uhn.fhir.rest.annotation.Delete;
 import ca.uhn.fhir.rest.annotation.IdParam;
 import ca.uhn.fhir.rest.annotation.IncludeParam;
 import ca.uhn.fhir.rest.annotation.OptionalParam;
@@ -23,6 +25,7 @@ import ca.uhn.fhir.rest.annotation.Read;
 import ca.uhn.fhir.rest.annotation.ResourceParam;
 import ca.uhn.fhir.rest.annotation.Search;
 import ca.uhn.fhir.rest.annotation.Sort;
+import ca.uhn.fhir.rest.annotation.Update;
 import ca.uhn.fhir.rest.api.MethodOutcome;
 import ca.uhn.fhir.rest.api.SortSpec;
 import ca.uhn.fhir.rest.api.server.IBundleProvider;
@@ -71,11 +74,13 @@ public class MedicationDispenseFhirResourceProvider implements IResourceProvider
 		return medicationDispense;
 	}
 	
+	@Create
 	public MethodOutcome createMedicationDispense(@ResourceParam MedicationDispense mDispense) {
 		MedicationDispense medicationDispense = fhirMedicationDispenseService.create(mDispense);
 		return FhirProviderUtils.buildCreate(medicationDispense);
 	}
-	
+
+	@Update
 	public MethodOutcome updateMedicationDispense(@IdParam IdType id, @ResourceParam MedicationDispense mDispense) {
 		if (id == null || id.getIdPart() == null) {
 			throw new InvalidRequestException("id must be specified to update resource");
@@ -84,7 +89,8 @@ public class MedicationDispenseFhirResourceProvider implements IResourceProvider
 		MedicationDispense medicationDispense = fhirMedicationDispenseService.update(id.getIdPart(), mDispense);
 		return FhirProviderUtils.buildUpdate(medicationDispense);
 	}
-	
+
+	@Delete
 	public OperationOutcome deleteMedicationDispense(@IdParam IdType id) {
 		fhirMedicationDispenseService.delete(id.getIdPart());
 		return FhirProviderUtils.buildDeleteR4();
