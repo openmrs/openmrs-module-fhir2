7a3e1250560a976b29fb58ad593ffcfce185fd2d
herman <hermanmuhereza22@gmail.com>
2023-05-21 20:29:29 +0300

    fix test

--- integration-tests-2.2/src/test/java/org/openmrs/module/fhir2/provider/r4/PatientFhirResourceProvider_2_2IntegrationTest.java
+++ integration-tests-2.2/src/test/java/org/openmrs/module/fhir2/provider/r4/PatientFhirResourceProvider_2_2IntegrationTest.java
@@ -1,7 +1,27 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
 package org.openmrs.module.fhir2.provider.r4;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.openmrs.module.fhir2.api.util.GeneralUtils.inputStreamToString;
+
+import java.io.InputStream;
+import java.util.Objects;
 
 import lombok.AccessLevel;
 import lombok.Getter;
+import org.hl7.fhir.r4.model.Enumerations;
 import org.hl7.fhir.r4.model.Patient;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,17 +31,8 @@ import org.openmrs.module.fhir2.providers.r4.PatientFhirResourceProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpServletResponse;
 
-import java.io.InputStream;
-import java.util.Objects;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.openmrs.module.fhir2.api.util.GeneralUtils.inputStreamToString;
-
 public class PatientFhirResourceProvider_2_2IntegrationTest extends BaseFhirR4IntegrationTest<PatientFhirResourceProvider, Patient> {
+	
 	private static final String PATIENT_UUID = "61b38324-e2fd-4feb-95b7-9e9a2a4400df";
 	
 	private static final String PATIENT_DATA_XML = "org/openmrs/module/fhir2/api/dao/impl/FhirPatient_2_2_initial_data.xml";
@@ -30,12 +41,7 @@ public class PatientFhirResourceProvider_2_2IntegrationTest extends BaseFhirR4In
 	
 	@Getter(AccessLevel.PUBLIC)
 	@Autowired
-	private PatientFhirResourceProvider patientFhirResourceProvider;
-	
-	@Override
-	public PatientFhirResourceProvider getResourceProvider() {
-		return patientFhirResourceProvider;
-	}
+	private PatientFhirResourceProvider resourceProvider;
 	
 	@Before
 	@Override
@@ -53,7 +59,7 @@ public class PatientFhirResourceProvider_2_2IntegrationTest extends BaseFhirR4In
 		}
 		
 		MockHttpServletResponse response = patch("/Patient/" + PATIENT_UUID).jsonMergePatch(jsonPatientPatch)
-				.accept(BaseFhirIntegrationTest.FhirMediaTypes.JSON).go();
+		        .accept(FhirMediaTypes.JSON).go();
 		
 		assertThat(response, isOk());
 		assertThat(response, notNullValue());
@@ -65,7 +71,6 @@ public class PatientFhirResourceProvider_2_2IntegrationTest extends BaseFhirR4In
 		assertThat(patient, notNullValue());
 		assertThat(patient.getIdElement().getIdPart(), equalTo(PATIENT_UUID));
 		assertThat(patient, validResource());
-		
-		assertThat(patient.getGender(), is("F"));
+		assertThat(patient.getGender(), equalTo(Enumerations.AdministrativeGender.FEMALE));
 	}
 }
