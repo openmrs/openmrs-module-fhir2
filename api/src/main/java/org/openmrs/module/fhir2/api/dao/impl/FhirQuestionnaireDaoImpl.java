0fb444eca4db0ef318601e169af59421baa549d2
icrc-loliveira <loliveira@icrc.org>
2024-07-11 16:33:15 +0100

    Added support to questionnaires search

--- api/src/main/java/org/openmrs/module/fhir2/api/dao/impl/FhirQuestionnaireDaoImpl.java
+++ api/src/main/java/org/openmrs/module/fhir2/api/dao/impl/FhirQuestionnaireDaoImpl.java
@@ -9,27 +9,42 @@
  */
 package org.openmrs.module.fhir2.api.dao.impl;
 
-import static org.hibernate.criterion.Restrictions.eq;
-import static org.hibernate.criterion.Restrictions.in;
+import static org.hibernate.criterion.Restrictions.*;
 
 import javax.annotation.Nonnull;
+import javax.persistence.criteria.*;
+import javax.persistence.criteria.CriteriaBuilder;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import ca.uhn.fhir.rest.param.StringAndListParam;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.openmrs.Form;
+import org.openmrs.FormResource;
 import org.openmrs.module.fhir2.FhirConstants;
 import org.openmrs.module.fhir2.api.dao.FhirQuestionnaireDao;
 import org.openmrs.module.fhir2.api.search.param.SearchParameterMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 @Setter(AccessLevel.PACKAGE)
 public class FhirQuestionnaireDaoImpl extends BaseFhirDao<Form> implements FhirQuestionnaireDao {
+	
+	@Autowired
+	@Getter(AccessLevel.PUBLIC)
+	@Setter(AccessLevel.PUBLIC)
+	@Qualifier("sessionFactory")
+	private SessionFactory sessionFactory;
 	
 	@Override
 	public Form getQuestionnaireById(@Nonnull Integer id) {
@@ -41,6 +56,53 @@ public class FhirQuestionnaireDaoImpl extends BaseFhirDao<Form> implements FhirQ
 	@SuppressWarnings("unchecked")
 	public List<Form> getQuestionnairesByIds(@Nonnull Collection<Integer> ids) {
 		return getSessionFactory().getCurrentSession().createCriteria(Form.class).add(in("id", ids)).list();
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Form> getSearchResults(@Nonnull SearchParameterMap theParams) {
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+
+		// Create CriteriaBuilder
+		CriteriaBuilder builder = session.getCriteriaBuilder();
+
+		// Create CriteriaQuery for main query
+		CriteriaQuery<Form> query = builder.createQuery(Form.class);
+		Root<Form> formRoot = query.from(Form.class);
+		formRoot.alias("f");
+
+		// Create Subquery for FormResource
+		Subquery<Long> subquery = query.subquery(Long.class);
+		Root<FormResource> resourceRoot = subquery.from(FormResource.class);
+		subquery.select(resourceRoot.get("form").get("formId")); // Selecting formId to match in main query
+
+		// Add predicates to the subquery
+		List<Predicate> subqueryPredicates = new ArrayList<>();
+		//subqueryPredicates.add(builder.equal(resourceRoot.get("form").get("formId"), formRoot.get("formId")));
+		subqueryPredicates.add(builder.equal(resourceRoot.get("name"), FhirConstants.FHIR_QUESTIONNAIRE_TYPE));
+		subquery.where(builder.and(subqueryPredicates.toArray(new Predicate[0])));
+
+		// Main query predicates
+		List<Predicate> mainPredicates = new ArrayList<>();
+		mainPredicates.add(builder.equal(formRoot.get("retired"), false));
+		mainPredicates.add(builder.exists(subquery));
+
+		// Add predicates to the query
+		query.select(formRoot).where(builder.and(mainPredicates.toArray(new Predicate[0])));
+		
+		List<Form> results = session.createQuery(query).getResultList();
+		return results.stream().map(this::deproxyResult).collect(Collectors.toList());
+	}
+	
+	private Criteria getSearchResultCriteria(SearchParameterMap theParams) {
+		Criteria criteria = sessionFactory.getCurrentSession().createCriteria(typeToken.getRawType());
+		
+		handleRetireable(criteria);
+		
+		setupSearchParams(criteria, theParams);
+		
+		return criteria;
 	}
 	
 	@Override
