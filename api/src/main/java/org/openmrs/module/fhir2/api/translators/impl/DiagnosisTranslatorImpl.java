79cb2464f760050609994ea96b077a8e0ebdb7a9
Parth Panchal <40067366+parthfloyd@users.noreply.github.com>
2025-10-09 00:43:05 -0700

    Update api/src/main/java/org/openmrs/module/fhir2/api/translators/impl/DiagnosisTranslatorImpl.java
    Co-authored-by: Ian <52504170+ibacher@users.noreply.github.com>

--- api/src/main/java/org/openmrs/module/fhir2/api/translators/impl/DiagnosisTranslatorImpl.java
+++ api/src/main/java/org/openmrs/module/fhir2/api/translators/impl/DiagnosisTranslatorImpl.java
@@ -43,7 +43,6 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
 public class DiagnosisTranslatorImpl implements DiagnosisTranslator {
 	
 	@Getter(PROTECTED)
