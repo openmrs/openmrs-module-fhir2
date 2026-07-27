93270f48e0b85a529deb2208034c6cff96d429c8
Ian <ian.c.bacher@gmail.com>
2026-07-27 13:53:18 -0400

    Re-implement FhirTaskService in terms of FhirResourceHandlers

---
+++ api/src/main/java/org/openmrs/module/fhir2/api/handler/FhirTaskBackedTaskHandler.java
@@ -0,0 +1,74 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.fhir2.api.handler;
+
+import static lombok.AccessLevel.PROTECTED;
+import static org.openmrs.module.fhir2.FhirConstants.OPENMRS_FHIR_STRUCTURE_DEFINITION_PREFIX;
+
+import javax.annotation.Nonnull;
+
+import ca.uhn.fhir.rest.api.server.IBundleProvider;
+import lombok.Getter;
+import lombok.Setter;
+import org.hl7.fhir.r4.model.Task;
+import org.openmrs.module.fhir2.api.dao.FhirTaskDao;
+import org.openmrs.module.fhir2.api.search.SearchQuery;
+import org.openmrs.module.fhir2.api.search.SearchQueryInclude;
+import org.openmrs.module.fhir2.api.search.param.SearchParameterMap;
+import org.openmrs.module.fhir2.api.translators.TaskTranslator;
+import org.openmrs.module.fhir2.model.FhirTask;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * Default handler for the FHIR {@link Task} resource, mapping onto the OpenMRS {@link FhirTask}
+ * table. Single backing today — claims every incoming Task. Carries the CRUD wiring lifted from the
+ * original {@code FhirTaskServiceImpl}.
+ */
+@Component
+@Order(Ordered.LOWEST_PRECEDENCE)
+public class FhirTaskBackedTaskHandler extends BaseFhirResourceHandler<Task, FhirTask> implements FhirResourceHandler<Task> {
+	
+	private static final String IMPLICIT_PROFILE = OPENMRS_FHIR_STRUCTURE_DEFINITION_PREFIX + "/openmrs-task";
+	
+	@Getter(value = PROTECTED)
+	@Setter(value = PROTECTED, onMethod_ = @Autowired)
+	private FhirTaskDao dao;
+	
+	@Getter(value = PROTECTED)
+	@Setter(value = PROTECTED, onMethod_ = @Autowired)
+	private TaskTranslator translator;
+	
+	@Getter(value = PROTECTED)
+	@Setter(value = PROTECTED, onMethod_ = @Autowired)
+	private SearchQueryInclude<Task> searchQueryInclude;
+	
+	@Getter(value = PROTECTED)
+	@Setter(value = PROTECTED, onMethod_ = @Autowired)
+	private SearchQuery<FhirTask, Task, FhirTaskDao, TaskTranslator, SearchQueryInclude<Task>> searchQuery;
+	
+	@Nonnull
+	@Override
+	public String getImplicitProfile() {
+		return IMPLICIT_PROFILE;
+	}
+	
+	@Override
+	public boolean canHandle(@Nonnull Task resource) {
+		return true;
+	}
+	
+	@Override
+	public IBundleProvider search(@Nonnull SearchParameterMap params) {
+		return searchQuery.getQueryResults(params, dao, translator, searchQueryInclude);
+	}
+}
